91cce3d6bd5ef9dd8ff05669f00ff39c1f8047ca
Wyatt <wyattf@gmail.com>
2020-03-30 18:54:23 -0400

    improved UI, fixed some bugs.

--- app/game.js
+++ app/game.js
@@ -10,6 +10,7 @@ function defaultData(){
     timeStart: 0,
     timeID: 0,
     errors: 0,
+    onError: false,
     totalProblems: 3,
     userAnswer: "",
     state: 1,
@@ -18,118 +19,138 @@ function defaultData(){
 
 
 var app = new Vue({
-    el: '#app',
-    data: function() { return defaultData(); }, 
-    methods: {
+  el: '#app',
+  data: function() { return defaultData(); }, 
+  methods: {
 
-      startTimer: function(){
-        this.timeStart = new Date();
-        this.timeID = setInterval(this.updateTimer, 10);
-      },
+    startTimer: function(){
+      this.timeStart = new Date();
+      this.timeID = setInterval(this.updateTimer, 20);
+    },
 
-      stopTimer: function(){
-        clearInterval(this.timeID);
-      },
+    stopTimer: function(){
+      clearInterval(this.timeID);
+    },
 
-      updateTimer: function(){
-        var currentTime = new Date()
-        , timeElapsed = new Date(currentTime - this.timeStart)
-        , min = timeElapsed.getUTCMinutes()
-        , sec = timeElapsed.getUTCSeconds()
-        , ms = timeElapsed.getUTCMilliseconds();
-      
-        this.time = min + ":" + sec + ":" + ms;
-      },
+    updateTimer: function(){
+      var currentTime = new Date()
+      , timeElapsed = new Date(currentTime - this.timeStart)
+      , min = timeElapsed.getUTCMinutes()
+      , sec = timeElapsed.getUTCSeconds()
+      , ms = timeElapsed.getUTCMilliseconds();
+    
+      this.time = min + ":" + sec + ":" + ms;
+    },
 
-      checkAnswer: function() {
-        if (Number(this.userAnswer) == this.answer){
-          this.correct += 1;
-          if (this.correct >= this.totalProblems){
-            this.stopTimer();
-            this.state = 3;
-            this.correct = this.totalProblems; // fix double score bug
-          }else{
-            this.generateProblem();
-          }
+    checkAnswer: function() {
+      self = this;
+      if (Number(this.userAnswer) == this.answer){
+        this.correct += 1;
+        if (this.correct >= this.totalProblems){
+          this.stopTimer();
+          this.state = 3;
+          this.correct = this.totalProblems; // fix double score bug
         }else{
-          this.errors += 1;
+          this.generateProblem();
         }
-        
-      },
-
-      generateProblem: function(){
-        val = this.randomNumber(1,4);
-        a = 0;
-        b = 0;
-
-        switch (val) {
-          case 1:
-            this.operator = "-";
-            a = this.randomNumber(10,100);
-            b = this.randomNumber(1, a);
-            this.answer = a - b;
-            break;
-
-          case 2:
-            this.operator = "x";
-            a = this.randomNumber(2,15);
-            b = this.randomNumber(2,15);
-            this.answer = a * b;
-            break;
-
-          case 3:
-            this.operator = "&divide;"; // html entity for division
-            a = this.randomNumber(2,15);
-            b = this.randomNumber(2,10);
-            c = a * b;
-            this.answer = a;
-            a = c;
-            break;
-
-          default:
-            this.operator = "+";
-            a = this.randomNumber(1,50);
-            b = this.randomNumber(1,50);
-            this.answer = a + b;
-            break;
-        }
-
-        this.partA = a;
-        this.partB = b;
-        this.userAnswer = "";
-      },
-
-      resetBoard: function(){
-        def = defaultData();
-        Object.assign(this.$data, def);
-        this.startTimer();
-        this.generateProblem();
-        this.state = 2;
-      },
-
-      randomNumber : function(minimum, maximum){
-        return Math.floor(Math.random() * maximum) + minimum;
-      }, 
-
-      startGame: function(){
+      }else if(this.userAnswer != ""){
         self = this;
-        this.resetBoard();
-      
-        window.addEventListener("keypress", function(e){
-          pressed = String.fromCharCode(e.keyCode);
-          if(pressed == 'R' || pressed == 'r'){
-            self.resetBoard();
-          }else if(pressed == 'a'){ // used for testing
-            self.userAnswer = self.answer;
-          }else if(e.keyCode == '13'){
-            self.checkAnswer();
-          }
-        });
+        this.onError = true;
+        setTimeout(function(){
+          self.onError = false
+        }, 500);
+        this.userAnswer = "";
+        this.errors += 1;
       }
     },
 
-    mounted: function(){
-      
+    generateProblem: function(){
+      op = this.randomNumber(1,4); // pick random operation
+      a = 0;
+      b = 0;
+
+      switch (op) {
+        // subtraction
+        case 1:
+          this.operator = "-";
+          a = this.randomNumber(5,100);
+          b = this.randomNumber(1, a);
+          this.answer = a - b;
+          break;
+
+        // mulitplication
+        case 2:
+          this.operator = "x";
+          a = this.randomNumber(2,12);
+          b = this.randomNumber(2,12);
+          this.answer = a * b;
+          break;
+
+        // division
+        case 3:
+          this.operator = "&divide;"; // html entity for division
+          a = this.randomNumber(2,12);
+          b = this.randomNumber(2,10);
+          c = a * b;
+          this.answer = a;
+          a = c;
+          break;
+
+        // addition
+        default:
+          this.operator = "+";
+          a = this.randomNumber(1,50);
+          b = this.randomNumber(1,50);
+          this.answer = a + b;
+          break;
+      }
+
+      this.partA = a;
+      this.partB = b;
+      this.userAnswer = "";
+    },
+
+    resetBoard: function(){
+      this.stopTimer();
+      def = defaultData();
+      Object.assign(this.$data, def);
+      this.startTimer();
+      this.generateProblem();
+      this.state = 2;
+      // force focus on answer box
+      this.$nextTick(function () {
+        this.$refs.answerInput.focus()
+      })
+    },
+
+    randomNumber: function(minimum, maximum){
+      return Math.floor(Math.random() * maximum) + minimum;
+    }, 
+
+    startGame: function(){
+      self = this;
+      this.resetBoard();
+    
+      window.addEventListener("keypress", function(e){
+        pressed = String.fromCharCode(e.keyCode);
+        if(pressed == 'R' || pressed == 'r'){
+          self.resetBoard();
+        }else if(pressed == 'a'){ // used for testing, fills in answer
+          self.userAnswer = self.answer;
+        }else if(e.keyCode == '13'){
+          self.checkAnswer();
+        }
+      });
     }
+  },
+
+  mounted: function () {
+    self = this;
+    window.addEventListener("keypress", function(e){
+      if(e.keyCode == '32'){
+        self.startGame();
+      }
+    });
+  }
 
 })
